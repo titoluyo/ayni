e6398dc43bf03d1996b0cefd44d6560a55133552
Tito Luyo Murata <tluyo@hotmail.com>
2015-10-24 05:22:31 -0500

    configuracion desde RPI codigo python

--- bluetooth_serial.test.py
+++ bluetooth_serial.test.py
@@ -3,18 +3,38 @@
 import serial
 from time import sleep
 
-ending = chr(13) + chr(10);
+ending = chr(10);
 
 bluetoothSerial = serial.Serial("/dev/rfcomm1", baudrate=9600)
 
-command = "SN"; #send data
+command = "@$"
+command += chr(1)
+command += chr(8)
+command += chr(2)
+command += chr(5)
+command += chr(3)
+command += chr(6)
+command += chr(7)
+command += ending #send data
+
+cmdSend = "@>" + ending
+
 count = 1;
+
+
+#while True:
+print "C " + str(count) + ":"
+bluetoothSerial.write(command)
+response = bluetoothSerial.readline()
+print response
+count = count + 1
+sleep(3);
 
 while True:
 	print "C " + str(count) + ":"
-	bluetoothSerial.write(command + str(count) + ending)
+	bluetoothSerial.write(cmdSend)
 	response = bluetoothSerial.readline()
 	print response
 	count = count + 1
-#sleep(1);
+	sleep(0.5);
 
